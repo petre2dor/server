d2d8bde298d2e119f4c66f1c49af604552d2cc16
Carl Schwan <carl@carlschwan.eu>
2022-01-31 16:52:24 +0100

    Profile dav requests
    Signed-off-by: Carl Schwan <carl@carlschwan.eu>

--- apps/dav/lib/Server.php
+++ apps/dav/lib/Server.php
@@ -35,6 +35,12 @@
 namespace OCA\DAV;
 
 use OCA\DAV\Connector\Sabre\RequestIdHeaderPlugin;
+use OC\Profiler\Profiler;
+use OC\Profiler\RoutingDataCollector;
+use OCA\DAV\Profiler\ProfilerPlugin;
+use OCP\AppFramework\Http\Response;
+use OCP\Diagnostics\IEventLogger;
+use OCP\IConfig;
 use Psr\Log\LoggerInterface;
 use OCA\DAV\AppInfo\PluginManager;
 use OCA\DAV\CalDAV\BirthdayService;
@@ -87,7 +93,19 @@ class Server {
 	/** @var Connector\Sabre\Server  */
 	public $server;
 
+	/** @var Profiler */
+	private $profiler;
+
 	public function __construct(IRequest $request, $baseUri) {
+		$this->profiler = \OC::$server->get(Profiler::class);
+		$config = \OC::$server->get(IConfig::class);
+		$this->profiler->setEnabled($config->getSystemValue('debug', false));
+		if ($this->profiler->isEnabled()) {
+			/** @var IEventLogger $eventLogger */
+			$eventLogger = \OC::$server->get(IEventLogger::class);
+			$eventLogger->start('runtime', 'DAV Runtime');
+		}
+
 		$this->request = $request;
 		$this->baseUri = $baseUri;
 		$logger = \OC::$server->getLogger();
@@ -114,6 +132,8 @@ class Server {
 		$this->server->httpRequest->setUrl($this->request->getRequestUri());
 		$this->server->setBaseUri($this->baseUri);
 
+
+		$this->server->addPlugin(new ProfilerPlugin($this->profiler));
 		$this->server->addPlugin(new BlockLegacyClientPlugin(\OC::$server->getConfig()));
 		$this->server->addPlugin(new AnonymousOptionsPlugin());
 		$authPlugin = new Plugin();
@@ -338,6 +358,13 @@ class Server {
 
 	public function exec() {
 		$this->server->exec();
+		if ($this->profiler->isEnabled()) {
+			/** @var IEventLogger $eventLogger */
+			$eventLogger = \OC::$server->get(IEventLogger::class);
+			$eventLogger->end('runtime');
+			$profile = $this->profiler->collect(\OC::$server->get(IRequest::class), new Response());
+			$this->profiler->saveProfile($profile);
+		}
 	}
 
 	private function requestIsForSubtree(array $subTrees): bool {
