06297b53474766a9275fc8ef8c6ec1325be966e9
Carl Schwan <carl@carlschwan.eu>
2022-01-31 18:58:25 +0100

    Use nextcloud request token instead of a new one
    Signed-off-by: Carl Schwan <carl@carlschwan.eu>

--- apps/dav/lib/Server.php
+++ apps/dav/lib/Server.php
@@ -36,7 +36,6 @@ namespace OCA\DAV;
 
 use OCA\DAV\Connector\Sabre\RequestIdHeaderPlugin;
 use OC\Profiler\Profiler;
-use OC\Profiler\RoutingDataCollector;
 use OCA\DAV\Profiler\ProfilerPlugin;
 use OCP\AppFramework\Http\Response;
 use OCP\Diagnostics\IEventLogger;
@@ -133,7 +132,7 @@ class Server {
 		$this->server->setBaseUri($this->baseUri);
 
 
-		$this->server->addPlugin(new ProfilerPlugin($this->profiler));
+		$this->server->addPlugin(new ProfilerPlugin($this->request, $this->profiler));
 		$this->server->addPlugin(new BlockLegacyClientPlugin(\OC::$server->getConfig()));
 		$this->server->addPlugin(new AnonymousOptionsPlugin());
 		$authPlugin = new Plugin();
